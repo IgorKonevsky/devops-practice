43441476249e444f5b47a93a21a5d574213abc2f
Igor Konevskii <i.konevskii@astondevs.ru>
2024-04-14 15:32:55 +0300

    init

---
+++ src/main/java/ru/igoryamba/devopspractice/controller/TestController.java
@@ -0,0 +1,9 @@
+package ru.igoryamba.devopspractice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+}
---
+++ src/test/java/ru/igoryamba/devopspractice/DevopsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.igoryamba.devopspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
